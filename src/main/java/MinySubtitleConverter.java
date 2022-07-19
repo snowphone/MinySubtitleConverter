785455d0656dfbf15b57358378bfe0b926a68b39
Junoh Moon <mjo970625@gmail.com>
2022-07-19 15:37:28 +0900

    Add argument parser

--- src/main/java/MinySubtitleConverter.java
+++ src/main/java/MinySubtitleConverter.java
@@ -2,11 +2,14 @@
  * Decompiled with CFR 0.150.
  */
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.myhyuny.MinySubtitleConverter.MainFrame;
 import com.myhyuny.MinySubtitleConverter.SubtitleConverter;
 
 import java.io.File;
 import java.lang.reflect.InvocationTargetException;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -14,18 +17,41 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class MinySubtitleConverter {
+    @Parameter(names = {"--help", "-h"}, help = true)
+    private boolean help = false;
+
+    @Parameter(description = "Subtitles...")
+    private List<String> uriList = new ArrayList<>();
+
     public static void main(String[] args) throws InterruptedException {
-        if (args.length < 1) {
+        var main = new MinySubtitleConverter();
+        var jcmd = JCommander.newBuilder()
+                .addObject(main)
+                .build();
+
+        jcmd.parse(args);
+        if (main.help) {
+            jcmd.setProgramName("MinySubtitleConverter");
+            jcmd.usage();
+            return;
+        }
+        main.run();
+    }
+
+    void run() throws InterruptedException {
+        if (this.uriList.isEmpty()) {
             try {
-                Class<?> c = Class.forName("com.myhyuny.MinySubtitleConverter.MainFrame");
-                c.getMethod("setVisible", Boolean.TYPE).invoke(c.getDeclaredConstructor().newInstance(), true);
-            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
+                Class<?> c = MainFrame.class;
+                c.getMethod("setVisible", Boolean.TYPE)
+                        .invoke(c.getDeclaredConstructor().newInstance(), true);
+            } catch (InstantiationException | IllegalAccessException | NoSuchMethodException |
                      InvocationTargetException e) {
                 e.printStackTrace();
             }
             return;
         }
-        List<File> list = Arrays.stream(args)
+        List<File> list = this.uriList
+                .stream()
                 .filter(uri -> SubtitleConverter.PATTERN_FILE_EXTENTION.matcher(uri).find())
                 .map(File::new)
                 .filter(File::isFile)
